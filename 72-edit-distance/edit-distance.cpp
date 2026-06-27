6a4f251941d0e0f1d8f67e05a4d6266ccce44f83
Rishab Thapar <thaparrishab21@gmail.com>
2026-06-27 17:29:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (98.79%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -16,21 +16,22 @@ public:
     int minDistance(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=n;i++) dp[i][0]=i;
-        for(int j=1;j<=m;j++) dp[0][j]=j;
+        vector<int>prev(m+1,0),curr(m+1,0);
+        for(int j=1;j<=m;j++) prev[j]=j;
         for(int i=1;i<=n;i++){
+            curr[0]=i;
             for(int j=1;j<=m;j++){
-                if(s[i-1]==t[j-1]) dp[i][j]=dp[i-1][j-1];
+                if(s[i-1]==t[j-1]) curr[j]=prev[j-1];
                 else{
-                    dp[i][j] = 1 + min({
-                        dp[i-1][j],     // delete
-                        dp[i-1][j-1],   // replace
-                        dp[i][j-1]      // insert
+                    curr[j] = 1 + min({
+                        prev[j],     // delete
+                        prev[j-1],   // replace
+                        curr[j-1]      // insert
                     });
                 }
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
