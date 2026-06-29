ea382daf3faa59224d77b9dd22b598483bae871c
Rishab Thapar <thaparrishab21@gmail.com>
2026-06-29 17:09:34 +0530

    Time: 2 ms (84.95%) | Memory: 14.4 MB (79.99%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
+    int f5(int ind,int buy,int n,int cap,vector<int>& prices,vector<vector<vector<int>>>&dp){
+        if(ind==n || cap==0) return 0;
+        if(dp[ind][buy][cap]!=-1) return dp[ind][buy][cap];
+        if(buy){
+            return dp[ind][buy][cap]=max(-prices[ind]+f5(ind+1,0,n,cap,prices,dp),
+                                    f5(ind+1,1,n,cap,prices,dp));
+        }
+        else{
+            return dp[ind][buy][cap]=max(prices[ind]+f5(ind+1,1,n,cap-1,prices,dp),
+                                    f5(ind+1,0,n,cap,prices,dp));
+        }
+    }
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<double>>>dp(n+1,vector<vector<double>>(2,vector<double>(k+1,0)));
+        vector<vector<int>>ahead(2,vector<int>(k+1,0));
+        vector<vector<int>>curr(2,vector<int>(k+1,0));
         for(int ind=n-1;ind>=0;ind--){
             for(int buy=0;buy<2;buy++){
                 for(int cap=1;cap<=k;cap++){
                     if(buy){
-                        dp[ind][buy][cap]=max(-prices[ind]+dp[ind+1][0][cap],
-                                                dp[ind+1][1][cap]);
+                        curr[buy][cap]=max(-prices[ind]+ahead[0][cap],
+                                                ahead[1][cap]);
                     }
                     else{
-                        dp[ind][buy][cap]=max(prices[ind]+dp[ind+1][1][cap-1],
-                                                dp[ind+1][0][cap]);
+                        curr[buy][cap]=max(prices[ind]+ahead[1][cap-1],
+                                                ahead[0][cap]);
                     }
                 }
+                ahead=curr;
             }
         }
-        return (int)dp[0][1][k];
+        return ahead[1][k];
     }
 };
