20de16d0964dda05ca57667e9b67833241ab3673
Rishab Thapar <thaparrishab21@gmail.com>
2026-06-30 11:54:36 +0530

    Time: 3 ms (33.03%) | Memory: 16.2 MB (51.70%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -10,7 +10,11 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return f5(0,1,n,prices,dp);
+        vector<vector<int>>dp(n+2,vector<int>(2,-0));
+        for(int ind=n-1;ind>=0;ind--){
+            dp[ind][1]=max(-prices[ind]+dp[ind+1][0],dp[ind+1][1]);
+            dp[ind][0]=max(prices[ind]+dp[ind+2][1],dp[ind+1][0]);
+        }
+        return dp[0][1];
     }
 };
