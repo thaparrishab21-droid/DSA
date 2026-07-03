d5a2a00dc82366d1bfcdefc0b8c88af2b35d266f
Rishab Thapar <thaparrishab21@gmail.com>
2026-07-03 17:54:15 +0530

    Time: 1756 ms (16.33%) | Memory: 9.4 MB (66.29%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPalindrome(int i,int j,string &s){
+        while(i<j){
+            if(s[i]!=s[j]) return false;
+            else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+    int f10(int i,int n,string &s,vector<int>&dp){
+        if(i==n) return 0;
+        int minCost=INT_MAX;
+        if(dp[i]!=-1) return dp[i];
+        for(int j=i;j<n;j++){
+            if(isPalindrome(i,j,s)){
+                int cost=1+f10(j+1,n,s,dp);
+                minCost=min(minCost,cost);
+            }
+        }
+        return dp[i]=minCost;
+    }
+    int minCut(string s) {
+        int n=s.size();
+        vector<int>dp(n,-1);
+        return f10(0,n,s,dp)-1;
+    }
+};
