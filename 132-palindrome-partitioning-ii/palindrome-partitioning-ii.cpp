15e9e4aaae4931d2477119a340c60e4bb0eac014
Rishab Thapar <thaparrishab21@gmail.com>
2026-07-03 18:01:28 +0530

    Time: 1506 ms (38.06%) | Memory: 9.3 MB (86.72%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -24,7 +24,17 @@ public:
     }
     int minCut(string s) {
         int n=s.size();
-        vector<int>dp(n,-1);
-        return f10(0,n,s,dp)-1;
+        vector<int>dp(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            int minCost=INT_MAX;
+            for(int j=i;j<n;j++){
+                if(isPalindrome(i,j,s)){
+                    int cost=1+dp[j+1];
+                    minCost=min(minCost,cost);
+                }
+            }
+            dp[i]=minCost;
+        }
+        return dp[0]-1;
     }
 };
