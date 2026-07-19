ef667dbbc98eefcc929f169256785354f7bbe479
Rishab Thapar <thaparrishab21@gmail.com>
2026-07-19 12:44:17 +0530

    Time: 0 ms (100.00%) | Memory: 28.6 MB (22.85%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         vector<int>ans(n);
         stack<int>st;
-        for(int i=2*n-1;i>=0;i--){
+        for(int i=2*n-2;i>=0;i--){
             int ind=i%n;
             while(!st.empty() && st.top()<=nums[ind]){
                 st.pop();
