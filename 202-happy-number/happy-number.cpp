4829b8150db032f54473c83ca1067229e7e390bf
Rishab Thapar <thaparrishab21@gmail.com>
2026-08-06 14:54:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (22.79%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+
+        unordered_set<int> vis;
+
+        while (n != 1 && vis.find(n) == vis.end()) {
+
+            vis.insert(n);
+
+            int sum = 0;
+
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+
+            n = sum;
+        }
+
+        return n == 1;
+    }
+};
