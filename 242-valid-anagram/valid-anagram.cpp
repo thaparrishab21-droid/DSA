6f9ebf390ce7adba427e67bd3adcd84ad8985a53
Rishab Thapar <thaparrishab21@gmail.com>
2026-08-06 14:24:31 +0530

    Time: 4 ms (34.37%) | Memory: 9.7 MB (76.55%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) return false;
-        unordered_map<int,int>mp;
-        for(char c: s){
-            mp[c-'a']++;
-        }
-        for(char c:t){
-            if(mp.find(c-'a')==mp.end() || mp[c-'a'] == 0) return false;
-            else{
-                mp[c-'a']--;
-            }
-        }
-        return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
