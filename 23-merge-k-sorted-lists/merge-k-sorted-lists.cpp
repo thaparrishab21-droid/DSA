cd6db07b5f2abde2e0c5c3c340dbf61d29e4803c
Rishab Thapar <thaparrishab21@gmail.com>
2026-08-08 16:16:30 +0530

    Time: 108 ms (7.46%) | Memory: 18.7 MB (39.11%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,39 +1,51 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
+
+    // Merge two sorted linked lists
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if (!l1) return l2;
-        if (!l2) return l1;
 
-        if (l1->val < l2->val) {
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
-        } else {
-            l2->next = mergeTwoLists(l1, l2->next);
-            return l2;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+
+        while (l1 != NULL && l2 != NULL) {
+
+            if (l1->val <= l2->val) {
+                temp->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                temp->next = l2;
+                l2 = l2->next;
+            }
+
+            temp = temp->next;
         }
+
+        // If l1 still has nodes
+        if (l1 != NULL) {
+            temp->next = l1;
+        }
+
+        // If l2 still has nodes
+        if (l2 != NULL) {
+            temp->next = l2;
+        }
+
+        return dummy->next;
     }
 
+
+    // Merge K sorted linked lists
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty()) return nullptr;
-        return divideAndConquer(lists, 0, lists.size() - 1);
-    }
 
-    ListNode* divideAndConquer(vector<ListNode*>& lists, int left, int right) {
-        if (left == right) return lists[left];
+        ListNode* ans = NULL;
 
-        int mid = left + (right - left) / 2;
-        ListNode* l1 = divideAndConquer(lists, left, mid);
-        ListNode* l2 = divideAndConquer(lists, mid + 1, right);
-        return mergeTwoLists(l1, l2);
+        for (int i = 0; i < lists.size(); i++) {
+
+            ans = mergeTwoLists(ans, lists[i]);
+
+        }
+
+        return ans;
     }
 };
